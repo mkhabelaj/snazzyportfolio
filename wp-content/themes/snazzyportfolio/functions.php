005f47990b1f9ae04612b7ef7b6ab0f95a7d97c7
Jackson Mkhabela <jacksonmkhabela@Jacksons-MacBook-Pro.local>
2024-10-17 06:56:51 -0700

    auto register a blocks

--- wp-content/themes/snazzyportfolio/functions.php
+++ wp-content/themes/snazzyportfolio/functions.php
@@ -26,32 +26,55 @@ function snazzyportfolio_features()
 }
 add_action('after_setup_theme', 'snazzyportfolio_features');
 
+
+// TODO: Move this to a separate file && Add error handling
+function auto_block_register($DIR, $build_dir, $src_dir)
+{
+
+  function get_paths($path)
+  {
+    $dir = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path), RecursiveIteratorIterator::LEAVES_ONLY);
+
+    $paths = [];
+    $rel = [];
+    foreach ($dir as $file) {
+      if ($file->isFile() && $file->getFilename() === 'block.json') {
+        $relesive_path = substr($file->getPath(), strlen($path));
+        $paths[] = ['path' => $file->getPath(), 'rel' => $relesive_path];
+        $rel[] = $relesive_path;
+      }
+    }
+
+    return [
+      'paths' => $paths,
+      'rel' => $rel
+    ];
+  }
+  $json_build_paths = get_paths($DIR . $build_dir);
+  $json_src_paths = get_paths($DIR . $src_dir);
+
+  $reconciled_paths = array_filter($json_build_paths['paths'], function ($json_path) use ($json_src_paths) {
+    return in_array($json_path['rel'], $json_src_paths['rel']);
+  });
+
+  $reconciled_paths = array_map(function ($json_path) {
+    return $json_path['path'];
+  }, $reconciled_paths);
+
+  foreach ($reconciled_paths as $json_path) {
+    register_block_type_from_metadata($json_path);
+  }
+}
+
 // snazzyportfolio_blocks
 
 function snazzyportfolio_blocks()
 {
-  register_block_type_from_metadata(__DIR__ . '/build/intro');
-  register_block_type_from_metadata(__DIR__ . '/build/introheadline');
-  register_block_type_from_metadata(__DIR__ . '/build/introparagraph');
-  register_block_type_from_metadata(__DIR__ . '/build/introsubheadline');
-  register_block_type_from_metadata(__DIR__ . '/build/singleproject');
-  register_block_type_from_metadata(__DIR__ . '/build/projecttitle');
-  register_block_type_from_metadata(__DIR__ . '/build/projectsubtitle');
-  register_block_type_from_metadata(__DIR__ . '/build/projectexcerpt');
-  register_block_type_from_metadata(__DIR__ . '/build/projectparagraph');
-  register_block_type_from_metadata(__DIR__ . '/build/projectimage');
-  register_block_type_from_metadata(__DIR__ . '/build/projectlayout');
-  register_block_type_from_metadata(__DIR__ . '/build/projectlayoutmaincontent');
-  register_block_type_from_metadata(__DIR__ . '/build/projectlayoutsidecontent');
-  register_block_type_from_metadata(__DIR__ . '/build/projecttaglist');
-  register_block_type_from_metadata(__DIR__ . '/build/tag');
-  register_block_type_from_metadata(__DIR__ . '/build/projectgrid');
-  register_block_type_from_metadata(__DIR__ . '/build/projectarchive');
-  register_block_type_from_metadata(__DIR__ . '/build/timeline');
-  register_block_type_from_metadata(__DIR__ . '/build/projecttimeline');
+  auto_block_register(__DIR__, '/build', '/src');
 }
 
 add_action('init', 'snazzyportfolio_blocks');
+
 
 // function for the main body content
 
