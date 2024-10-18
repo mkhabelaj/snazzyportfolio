bef20652987b6ee285953b8fd3bb113c094ede16
Jackson Mkhabela <jacksonmkhabela@Jacksons-MacBook-Pro.local>
2024-10-18 04:12:28 -0700

    Add menu

---
+++ wp-content/themes/snazzyportfolio/build/grid/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks'), 'version' => '7b22ee2705b293d8a1bc');
---
+++ wp-content/themes/snazzyportfolio/build/header/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks'), 'version' => '77ff90b842e0e9b8ff5f');
