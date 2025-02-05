81cb50a2af289a2e3e5d5e232e62501eca9a4567
Jackson Mkhabela <jacksonmkhabela@Jacksons-MacBook-Pro.local>
2025-02-04 19:40:06 -0800

    Add fixed with for project grid

--- snazzyportfolio/template-parts/grid.php
+++ snazzyportfolio/template-parts/grid.php
@@ -11,12 +11,15 @@ $tagList = (ExtendedProjectMetaModel::instance(new ExtendedProjectMetaModel($pos
           sp-rounded-md sp-overflow-hidden sp-border-slate-600
           hover:sp-border-teal-600 hover:sp-border-2
           sp-max-w-sm
+          sp-w-72
+          sp-h-80
+          md:sp-w-80
           sp-transition-transform sp-duration-200 sp-ease-out hover:sp-scale-105 sp-min-h-64 md:sp-min-h-96">
     <img src="<?php the_post_thumbnail_url("projectLandscapeSm"); ?>" alt="portfolio image"
       class="sp-w-full sp-h-36 md:sp-h-48 sp-object-cover sp-cursor-pointer">
     <div class="sp-w-full sp-p-4">
       <h2 class="sp-text-lg md:sp-text-xl sp-mb-2 md:sp-mb-3 sp-font-semibold sp-text-center sp-text-slate-200"><?php the_title(); ?></h2>
-      <p class="sp-flex sp-flex-wrap sp-gap-2 sp-flex-row sp-items-center sp-justify-start sp-text-xs md:sp-text-sm">
+      <p class="sp-flex sp-flex-wrap sp-gap-2 sp-flex-row sp-items-center sp-justify-center sp-text-xs md:sp-text-sm">
         <?php if (!empty($tagList)) : ?>
           <?php foreach ($tagList as $tag) : ?>
             <span class="sp-border-white sp-inline-block sp-px-2 sp-py-1 sp-font-semibold sp-bg-teal-400/10 sp-text-teal-300 sp-leading-5 sp-rounded-full">
