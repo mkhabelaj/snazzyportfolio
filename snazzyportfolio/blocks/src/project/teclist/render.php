dec27c2867e421b05ca9e815558e3a27d957a3f2
Jackson Mkhabela <jacksonmkhabela@Jacksons-MacBook-Pro.local>
2025-02-09 04:03:28 -0800

    Remove sticky tag

--- snazzyportfolio/blocks/src/project/teclist/render.php
+++ snazzyportfolio/blocks/src/project/teclist/render.php
@@ -5,7 +5,7 @@ use Jcodify\Snazzyportfolio\Wordpress\ORM\Models\Extended\ExtendedProjectMetaMod
 $post_id = get_the_ID();
 $tagList = (ExtendedProjectMetaModel::instance(new ExtendedProjectMetaModel($post_id)))->taxonomyTechnology->getValue();
 ?>
-<div class="sp-flex sp-flex-col sp-gap-4 sp-sticky sp-top-4">
+<div class="sp-flex sp-flex-col sp-gap-4">
   <div>
     <h2 class="sp-text-2xl 
       sp-font-bold 
@@ -18,7 +18,7 @@ $tagList = (ExtendedProjectMetaModel::instance(new ExtendedProjectMetaModel($pos
     <p class="sp-flex sp-flex-wrap sp-gap-2 sp-flex-row sp-items-center sp-justify-start sp-text-xs md:sp-text-sm">
       <?php
       if (!empty($tagList)) :
-        foreach ($tagList as $tag): ?>
+          foreach ($tagList as $tag): ?>
           <a href="<?= get_term_link($tag->term_id) ?>" class="sp-text-slate-300 hover:sp-text-slate-200 sp-font-semibold sp-underline">
             <span class="sp-border-white 
                          sp-inline-block sp-px-2 sp-py-1 sp-font-semibold 
