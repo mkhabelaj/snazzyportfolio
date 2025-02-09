9d5555b539cf173618243774d62aeb40bf0afecf
Jackson Mkhabela <jacksonmkhabela@Jacksons-MacBook-Pro.local>
2025-02-09 04:16:01 -0800

    allow link highlighting in paragraphs

--- snazzyportfolio/blocks/src/project/paragraph/render.php
+++ snazzyportfolio/blocks/src/project/paragraph/render.php
@@ -8,7 +8,7 @@ $classe_map = [
 $size = $attributes['size'];
 $text_size_class = $classe_map[$size];
 $text = $attributes['text'];
-$wrapper_attributes =  "$text_size_class sp-md:sp-max-w-[650px] sp-whitespace-pre-wrap sp-my-5 sp-leading-tight";
+$wrapper_attributes =  "$text_size_class sp-md:sp-max-w-[650px] sp-whitespace-pre-wrap sp-my-5 sp-leading-tight project-paragraph";
 ?>
 <p class="<?= $wrapper_attributes; ?>"><?= $text; ?></p>
 
