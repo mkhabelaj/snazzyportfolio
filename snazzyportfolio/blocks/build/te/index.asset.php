79c497c67e46fe8aa6a7802ce2a9d8ebf017eccd
Jackson Mkhabela <jacksonmkhabela@Jacksons-MacBook-Pro.local>
2025-02-10 11:20:14 -0800

    remove unused block

--- snazzyportfolio/blocks/build/te/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => '34a9cf2d9694b05e6f28');
--- snazzyportfolio/blocks/build/te/render.php
+++
@@ -1,9 +0,0 @@
-<?php
-$attributes;
-$container_classes = $attributes['container'];
-$content_classes = $attributes['content'];
-?>
-<div class="<?php echo $container_classes; ?>">
-  <p class="sp-text-md sp-italic sp-whitespace-pre-wrap sp-my-5 sp-font-medium sp-leading-tight <?php echo $content_classes; ?>"
-  ><?php echo get_the_excerpt();?></p>
-</div>
