a945d38775b741f84a2844bee0ab0b0af882bab6
Jackson Mkhabela <jacksonmkhabela@Jacksons-MacBook-Pro.local>
2025-02-22 03:24:29 -0800

    Make dropdown inconspicuous

--- snazzyportfolio/blocks/build/dropdown/render.php
+++ snazzyportfolio/blocks/build/dropdown/render.php
@@ -3,6 +3,7 @@
 use Nzuridesigns\WPUtility\ThemeIconManager;
 
 $iconManager = ThemeIconManager::getInstance();
+$iconManager->setDefaultAttributes(['width' => '20', 'height' => '20']);
 $chevDow = $iconManager->render('circle-chevron-down');
 $chevUp = $iconManager->render('circle-chevron-up');
 $title = $attributes['title'];
@@ -12,18 +13,26 @@ $initialOpen = $attributes['initialOpen'];
 <div 
   data-wp-interactive="snazzyDropdown"
   <?= wp_interactivity_data_wp_context(['navIsOpen' => $initialOpen]) ?>
-  class="sp-mb-4"
+  class="sp-mb-2 
+    group
+    sp-text-slate-300
+    hover:sp-text-teal-300
+  "
 >
-  <div class="sp-bg-gray-800 sp-p-3 sp-flex sp-flex-row sp-justify-start sp-gap-2 sp-items-center sp-font-bold"> 
-    <!--icon-->
-    <span class="sp-flex sp-flex-row sp-gap-2 sp-items-center hover:sp-cursor-pointer hover:sp-text-teal-300"
+  <div class=" sp-bg-gray-800 sp-p-1 sp-rounded 
+    sp-flex sp-flex-row sp-justify-start sp-gap-2 sp-items-center 
+    sp-font-bold hover:sp-cursor-pointer 
+    "
       data-wp-on--click="actions.toggleNav"
+  > 
+    <!--icon-->
+    <span class="sp-flex sp-flex-row sp-gap-2 sp-items-center " "
     >
       <span data-wp-bind--hidden="context.navIsOpen"><?php echo $chevDow; ?></span>
       <span data-wp-bind--hidden="!context.navIsOpen"><?php echo $chevUp; ?></span>
     </span>
    <!--header-->
-    <h3 class="sp-text-l  sp-text-slate-200 sp-uppercase">
+    <h3 class="sp-text-sm group-hover:sp-text-teal-300  sp-uppercase">
      <?= $title; ?>
     </h3>
   </div>
